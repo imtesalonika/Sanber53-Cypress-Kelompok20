31c5bc6aa3d3706a7b61d0c010a34b2b0cdda8ce
imtesalonika <tesalonilaprisdasitopu@gmail.com>
2024-02-05 20:43:36 +0700

    Menambah saucedemo

--- cypress/e2e/3-web/saucedemo/login.cy.js
+++ cypress/e2e/3-web/saucedemo/login.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login Functionality', () => {
+  it('Verify Failed Login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').clear().type('elvanisa')
+    cy.get('[name="password"]').clear().type('123456')
+    cy.get('#login-button').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+
+  describe('E2E Login to inventory', () => {
+
+    it('input valid username dan password then Login', () => {
+
+      cy.visit('https://www.saucedemo.com')
+
+      cy.get('#user-name').type("standard_user")
+
+      cy.get('#password').type("secret_sauce")
+
+      cy.contains("LOGIN").click()
+
+    })
+
   })
 })
