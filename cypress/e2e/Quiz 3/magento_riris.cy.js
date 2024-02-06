4f59bddd3ad2950857487884acd5e82d13948284
Hanny Yosephine <81764285+hannyyosephine16@users.noreply.github.com>
2024-02-06 10:27:01 +0700

    Menambah bagian riris

--- cypress/e2e/Quiz 3/magento_riris.cy.js
+++ cypress/e2e/Quiz 3/magento_riris.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("My Account", () => {
+  it("Edit Address berhasil", () => {
+    cy.visit(
+      "https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2xvZ291dFN1Y2Nlc3Mv/"
+    );
+  });
+});
