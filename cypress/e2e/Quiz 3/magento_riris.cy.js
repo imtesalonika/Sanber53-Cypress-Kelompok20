1767d73352f3def96f7231762f8cf6c27b0c01f8
Hanny Yosephine <81764285+hannyyosephine16@users.noreply.github.com>
2024-02-06 11:19:26 +0700

    menambah tugas riris

--- cypress/e2e/Quiz 3/magento_riris.cy.js
+++ cypress/e2e/Quiz 3/magento_riris.cy.js
@@ -1,7 +1,14 @@
-describe("My Account", () => {
-  it("Edit Address berhasil", () => {
+describe("Login", () => {
+  it("Login", () => {
     cy.visit(
-      "https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2xvZ291dFN1Y2Nlc3Mv/"
+      "https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/"
     );
+    cy.get("#email").type("yuyut@gmail.com");
+    cy.get(
+      ".login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass"
+    ).type("Riyroisi16-");
+    cy.get(
+      ".login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2"
+    ).click();
   });
 });
